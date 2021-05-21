7d9bf45a6c36b38d5d5cae8f36ee0c3ae833d15e
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-05-21 15:24:01 +0900

    edit→update→show のルーティング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(name:"a",email:"b", password:"a" )
     @user.save
     redirect_to user_path(@user.id)
   end
+
   def show
    @user = User.find_by(id: params[:id])
   end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(user_para)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+   def user_para
+    params.require(:user).permit(:name, :email)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :pictures
-  resources :users, only: [:edit, :new, :destroy, :create, :show]
+  resources :users, only: [:edit, :new, :destroy, :create, :show, :update]
 end
