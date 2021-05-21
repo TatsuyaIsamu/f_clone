d9ccd89019dfa5efbb48e4d4c55bfd4b45b615bc
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-05-21 14:59:15 +0900

    簡易的に new→create→show のルーティングでページ作成（試験のためテスト用アドレス使用 validation はコメントに

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
-  
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(name:"a",email:"b", password:"a" )
+    @user.save
+    redirect_to user_path(@user.id)
+  end
+  def show
+   @user = User.find_by(id: params[:id])
+  end
 end
