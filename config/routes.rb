bd9dbe3fdef129e4409c40dea833a8ce3dbb370b
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-05-22 12:44:34 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
         post :confirm
       end
   end
-  resources :sessions,  only: [:new, :create, :destrory]
+  resources :sessions,  only: [:new, :create, :destroy]
 end
