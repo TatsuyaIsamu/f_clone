9a822bfec67f3d4886ce0ef596e60b4c856bd684
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-05-22 13:58:00 +0900

    ログインの有無によるアクセス制限の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  before_action :login_required
+  before_action :forbid_login_user
+  private
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
+
+  def forbid_login_user
+    redirect_to "/" if current_user.present?
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :login_required, only: [:top]
+  skip_before_action :forbid_login_user, only: [:top]
   def top
   end
 end
